fd36d667d7d44710e4401d231d65741cda13b9de
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-19 22:06:37 +0000

    added models for allergens and course and migrated to the database

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Allergen, Course
+
+admin.site.register(Allergen)
+admin.site.register(Course)
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Allergen(models.Model):
+    DAIRY = 'Dairy'
+    EGGS = 'Eggs'
+    FISH = 'Fish'
+    SHELFISH = 'Shelfish'
+    NUTS = 'Nuts'
+    PEANUTS = 'Peanuts'
+    WHEAT = 'Wheat'
+    SOY = 'Soy'
+    ALLERGENS = [
+        (DAIRY, 'Dairy'),
+        (EGGS, 'Eggs'),
+        (FISH, 'Fish'),
+        (SHELFISH, 'Shelfish'),
+        (NUTS, 'Nuts'),
+        (PEANUTS, 'Peanuts'),
+        (WHEAT, 'Wheat'),
+        (SOY, 'Soy'),
+    ]
+    name = models.CharField(
+        ('Allergen name'),
+        max_length=20,
+        choices=ALLERGENS,
+        unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    price = models.IntegerField()
+    allergens = models.ManyToManyField(Allergen, blank=True)
+    slug = models.SlugField(blank=True, null=True, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+# https://stackoverflow.com/questions/837828/how-do-i-create-a-slug-in-django
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.name:
+            self.slug = slugify(self.name)
+        super(Course, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.name)
