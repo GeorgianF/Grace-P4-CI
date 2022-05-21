b0e56e7cc97f60a4f63b62456fef60ad2436dc34
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-21 12:54:13 +0000

    add filter, search, display options in the admin panel

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Course
 
-# Register your models here.
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'course_type')
+    list_filter = ('course_type', )
+    search_fields = ['name', 'description']
+
+
+admin.site.register(Course, CourseAdmin)
