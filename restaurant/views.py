d82ece90a881d41204fd57bf058c02173c9e9255
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-21 14:10:23 +0000

    add the main view and tested the output on the html file + css

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Course
 
 
 # Create your views here.
 
-
+class CourseListViews(ListView):
+    model = Course
+    queryset = Course.objects.order_by('course_type')
+    template_name = 'index.html'
