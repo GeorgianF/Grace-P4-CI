b2fd2c2984c328730544a9294be9e64b2a5d978e
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-22 18:07:32 +0000

    add id to footer to be able to hide it once the menu button is clicked + refactored JS function

--- static/js/script.js
+++ static/js/script.js
@@ -3,19 +3,22 @@
 const menuBtn = document.querySelector('.menu-btn');
 const navBar = document.querySelector('.nav-container');
 const siteWrapper = document.querySelector('site-wrapper');
-
+const footer = document.querySelector('#footer')
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
+
+menuBtn.addEventListener('click', hideContentShowNav);
+
+function hideContentShowNav() {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
     navBar.style.display = 'block';
-    
+    footer.classList.add('d-none');
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
     navBar.style.display = 'none';
+    footer.classList.remove('d-none');
   }
-});
-
+}
 
