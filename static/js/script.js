ebf2e4b12bd3d11728316a75003033b4bc067fb0
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-22 11:54:28 +0000

    add footer and styles to the base.html

--- static/js/script.js
+++ static/js/script.js
@@ -2,19 +2,20 @@
 
 const menuBtn = document.querySelector('.menu-btn');
 const navBar = document.querySelector('.nav-container');
+const hideContent = document.querySelector('.hide-content')
 
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
-    navBar.style.opacity = "0.7"
+    navBar.style.opacity = "0.7";
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
-    navBar.style.opacity = "0"
+    navBar.style.opacity = "0";
+
   }
 });
-
 
 
