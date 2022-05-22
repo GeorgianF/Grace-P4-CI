fe5c13dabf8c12cb2605a41dcb52049970485a6b
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-22 10:21:07 +0000

    add full screen nav bar and animation on the click event

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,20 @@
+// NAV BAR Animation JS
+
 const menuBtn = document.querySelector('.menu-btn');
+const navBar = document.querySelector('.nav-container');
+
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
+    navBar.style.opacity = "0.7"
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
+    navBar.style.opacity = "0"
   }
 });
+
+
+
