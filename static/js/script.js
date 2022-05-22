6e15a39654fe91599ac9a6e5967abe23de9d1d4d
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-22 14:20:17 +0000

    added changes to the nav bar because it did not cover the text

--- static/js/script.js
+++ static/js/script.js
@@ -2,19 +2,19 @@
 
 const menuBtn = document.querySelector('.menu-btn');
 const navBar = document.querySelector('.nav-container');
-const hideContent = document.querySelector('.hide-content')
+const siteWrapper = document.querySelector('site-wrapper');
 
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
-    navBar.style.opacity = "0.7";
+    navBar.style.display = 'block';
+    
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
-    navBar.style.opacity = "0";
-
+    navBar.style.display = 'none';
   }
 });
 
