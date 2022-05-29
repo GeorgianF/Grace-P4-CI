89f24ba6a4d5297f3ca4bb18ce6ca71fb5382ef4
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-29 18:15:33 +0000

    add content to the FAQ page + accordion style bootstrap classes

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,6 @@
 // NAV BAR Animation JS
 const menuBtn = document.querySelector('.menu-btn');
 const navBar = document.querySelector('.nav-container');
-const before = document.querySelector('.menu-btn__burger::before')
 let menuOpen = false;
 
 menuBtn.addEventListener('click', hideContentShowNav);
@@ -11,12 +10,10 @@ function hideContentShowNav() {
     menuBtn.classList.add('open');
     menuOpen = true;
     navBar.style.right = '0';
-    before.style.backgoundColor = "#fff"
 
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
     navBar.style.right = '-350px'
-    before.style.backgoundColor = "#000000"
   }
 }
