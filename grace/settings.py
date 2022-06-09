e9810c5733fc97332bc6e31277bc15e8dd927110
GeorgianF <georgianfurtuna@yahoo.com>
2022-06-09 20:46:23 +0000

    modify the settings.py file

--- grace/settings.py
+++ grace/settings.py
@@ -186,7 +186,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'grace.booking.ci@gmail.com'
-EMAIL_HOST_PASSWORD = 'CodeInstitute2022!'
-EMAIL_USE_TLS = True
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
